64c6229c4df258cc3b46bb1c851cdd04b59e6a50
Jon Wyatt <jon@zoetrope.io>
2017-09-14 13:33:36 +0100

    #3 Implement new zc-web <Icon/> component

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,9 +2,6 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 import Page from 'zc-web/components/Page'
-import buildingSVG from 'zc-web/icons/White/Building.svg'
-import faucetSVG from 'zc-web/icons/White/faucet.svg'
-import personSVG from 'zc-web/icons/White/person.svg'
 
 import Buildings from '../Buildings'
 
@@ -12,9 +9,9 @@ export default function App() {
   return (
     <Page
       navItems={[
-        { title: 'Buildings', icon: buildingSVG, route: '/buildings' },
-        { title: 'Outlets', icon: faucetSVG, route: '/outlets' },
-        { title: 'Account', icon: personSVG, route: '/account' },
+        { title: 'Buildings', icon: 'BUILDING', route: '/buildings' },
+        { title: 'Outlets', icon: 'FAUCET', route: '/outlets' },
+        { title: 'Account', icon: 'PERSON', route: '/account' },
       ]}
     >
       <Switch>
--- src/containers/Buildings/index.js
+++ src/containers/Buildings/index.js
@@ -1,19 +1,17 @@
 import React from 'react'
 
 import { Content } from 'zc-web/components'
-import addSVG from 'zc-web/icons/Dark Grey/add.svg'
-import printSVG from 'zc-web/icons/Dark Grey/print.svg'
 
 import { Buildings as Demo } from '../demo'
 
 
-export default function Buildings () {
+export default function Buildings() {
   return (
     <Content
       title="Buildings"
       actionItems={[
-        { title: 'Add building', icon: addSVG, action: () => {} },
-        { title: 'Print report', icon: printSVG, action: () => {} },
+        { title: 'Add building', icon: 'ADD', action: () => {} },
+        { title: 'Print report', icon: 'PRINT', action: () => {} },
       ]}
     >
       <Demo />
