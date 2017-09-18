150d091544992f07413ee8cf34550dfc36a62558
Jon Wyatt <jon@zoetrope.io>
2017-09-18 10:45:27 +0100

    #6 Update to latest zc-web - pass react router navigate action down as prop

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,24 +1,48 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router'
+import { push } from 'react-router-redux'
+import { Route, Switch, Redirect } from 'react-router-dom'
 
 import Page from 'zc-web/components/Page'
 
 import Buildings from '../Buildings'
 import Account from '../Account'
 
-export default function App() {
+
+function App({ navigate, location }) {
   return (
     <Page
+      activeRoute={location.pathname}
+      navigate={navigate}
       navItems={[
         { title: 'Buildings', icon: 'BUILDING', route: '/buildings' },
         { title: 'Outlets', icon: 'FAUCET', route: '/outlets' },
         { title: 'Account', icon: 'PERSON', route: '/account' },
       ]}
     >
+      <Redirect from="/" exact to="/buildings" />
       <Switch>
         <Route path="/buildings" component={Buildings} />
-        <Route path='/account' component={Account} />
+        <Route path="/account" component={Account} />
       </Switch>
     </Page>
   )
 }
+
+App.propTypes = {
+  navigate: PropTypes.func.isRequired,
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }).isRequired,
+}
+
+const mapDispatchToProps = dispatch => ({
+  navigate: route => dispatch(push(route)),
+})
+
+export default withRouter(connect(
+  null,
+  mapDispatchToProps,
+)(App))
