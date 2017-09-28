cc05e926c16a8cbd01256eb20b63885050d994b7
coxm <coxm@users.noreply.github.com>
2017-09-28 13:36:57 +0100

    Add demo list component

--- src/containers/Buildings/AllBuildings.js
+++ src/containers/Buildings/AllBuildings.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import { RowDefinition, ColumnDefinition } from 'griddle-react'
+
+import { TabContent } from 'zc-web/views'
+import { List } from 'zc-web/views'
+
+const randInt = (gte, lt) => (Math.random() * (lt - gte) + gte) | 0
+const randElem = (arr) => arr[randInt(0, arr.length)]
+const thumbnails = [0, 1, 2].map(i => `../../assets/images/building${i}.png`)
+const data = [
+  'Big Building',
+  'Medium Building',
+  'Small Building',
+  'Big Place',
+  'Medium Place',
+  'Small Place',
+  'Big House',
+  'Medium House',
+  'Small House',
+].map((name, id) => {
+  const outlets = randInt(10, 100)
+  return {
+    id,
+    name,
+    outlets,
+    atRisk: randInt(1, outlets),
+    thumbnail: randElem(thumbnails),
+  }
+})
 
 export default function AllBuildings() {
   return (
-    <div>
-      <h2>All buildings</h2>
-      <p>All the buildings.</p>
-    </div>
+    <List data={data}>
+      <RowDefinition>
+        <ColumnDefinition id="id" title="ID" />
+        <ColumnDefinition id="name" title="Name" />
+        <ColumnDefinition id="outlets" title="Outlets" />
+        <ColumnDefinition id="atRisk" title="At risk" />
+      </RowDefinition>
+    </List>
   )
 }
