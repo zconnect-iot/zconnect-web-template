3a4c2990ccc1a501eaf63aac916831094a468f7b
coxm <coxm@users.noreply.github.com>
2017-09-28 17:13:10 +0100

    Add demo usage of Card view

--- src/views/Building/index.js
+++ src/views/Building/index.js
@@ -2,29 +2,31 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import BEMHelper from 'react-bem-helper'
 
-import './style.scss'
+import { Icon } from 'zc-web/components'
+import { default as Card, classes } from 'zc-web/views/Card'
+import { StatCard } from 'zc-web/widgets'
 
-export const classes = new BEMHelper('Building')
+import style from './style.scss'
 
 const Building = props => (
-  <div {...classes()}>
-    <div {...classes('main')}>
-      <div {...classes('thumbnail')}>
+  <div className={style.Building}>
+    <Card>
+      <div {...classes('block', 'padded')}>
+        <div className={style.IconWrapper}>
+          <div className={style.WarningIcon}><Icon name="WARNING" /></div>
+        </div>
         <img src={props.thumbnail} alt={props.name} />
       </div>
-    </div>
 
-    <div {...classes('footer')}>
-      <div {...classes('notice')}>
-        <div {...classes('figure')}>{props.outlets}</div>
-        <span {...classes('description')}>Outlets</span>
+      <div {...classes('block', 'padded')}>
+        <span className={style.BuildingName}>{props.name}</span>
       </div>
 
-      <div {...classes('notice')}>
-        <div {...classes('figure', 'danger')}>{props.atRisk}</div>
-        <span {...classes('description', 'danger')}>At risk</span>
+      <div {...classes('block', ['dark', 'border-top'])}>
+        <StatCard inline invert figure={props.outlets} description="Outlets" />
+        <StatCard inline invert figure={props.atRisk} description="At risk" dangerFigure={props.atRisk > 0} />
       </div>
-    </div>
+    </Card>
   </div>
 )
 
