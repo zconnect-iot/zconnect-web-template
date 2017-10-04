b7bcaf04121ddf39429e5ae925986352e0d75d9c
coxm <coxm@users.noreply.github.com>
2017-10-04 14:50:48 +0100

    Add basic list demo

--- src/containers/Buildings/AllBuildings.js
+++ src/containers/Buildings/AllBuildings.js
@@ -1,10 +1,30 @@
 import React from 'react'
 
+import { List } from 'zc-web/views'
+
+const randInt = (gte, lt) => Math.floor((Math.random() * (lt - gte)) + gte)
+const data = [
+  'Big Building',
+  'Medium Building',
+  'Small Building',
+  'Big Place',
+  'Medium Place',
+  'Small Place',
+  'Big House',
+  'Medium House',
+  'Small House',
+].map((name, id) => {
+  const outlets = randInt(10, 100)
+  return {
+    id,
+    name,
+    outlets,
+    atRisk: randInt(1, outlets),
+  }
+})
+
+const columns = ['id', 'name', 'outlets', 'atRisk']
+
 export default function AllBuildings() {
-  return (
-    <div>
-      <h2>All buildings</h2>
-      <p>All the buildings.</p>
-    </div>
-  )
+  return <List data={data} columns={columns} />
 }
